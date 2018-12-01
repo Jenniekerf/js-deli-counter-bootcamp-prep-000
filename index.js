84b4130e97a089c170f1601ed481710b3cef4757
Jennie Kerfstedt <jenniekerf@gmail.com>
2018-12-01 17:43:06 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,22 @@
+function takeANumber(katzDeliLine,name) {
+  katzDeliLine.push(name);
+  return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`;
+}
+
+function nowServing(katzDeliLine) {
+  if(katzDeliLine>[]) {
+    return `Currently serving ${katzDeliLine.shift()}.`
+  }
+  else {
+    return "There is nobody waiting to be served!"
+  }
+}
+
+function currentLine(katzDeliLine) {
+  if (katzDeliLine > []) {
+    return `The line is currently ${katzDeliLine}`;
+    else {
+      return "This line i currently empty.";
+    }
+  }
+}
